e3a3610c380998f15bf470dfc363a4a5ec21ca38
duralie4 <antoncupin306@gmail.com>
2026-06-03 14:05:40 +0300

    Класс Recipe

---
+++ Recipe.py
@@ -0,0 +1,29 @@
+from Ingredient import Ingredient
+
+class Recipe:
+    def __init__(self, title: str, ingredients: list[Ingredient]):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        for existing in self.ingredients:
+            if existing == ingredient:
+                existing.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio: float) -> bool:
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio: float):
+        new_ingredients = []
+        for ingredient in self.ingredients:
+            new_ingredients.append(Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit))
+        return Recipe(self.title, new_ingredients)
+
+    def __len__(self) -> int:
+        return len(self.ingredients)
+
+    def __str__(self):
+        return self.title + "\n" + "\n".join(repr(i) for i in self.ingredients)
