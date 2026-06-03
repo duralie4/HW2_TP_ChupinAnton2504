6a99cf982a6dd9502bedfe770cdb74202b42fd35
duralie4 <antoncupin306@gmail.com>
2026-06-03 15:31:29 +0300

    Окончательно убрал рекурсию _quantity

--- Ingredient.py
+++ Ingredient.py
@@ -6,14 +6,14 @@ class Ingredient:
 
     @property
     def quantity(self) -> float:
-        return self.quantity
+        return self._quantity
 
     @quantity.setter
     def quantity(self, value : float) -> None:
         if value <= 0:
             raise ValueError("Количество должно быть положительным")
         else:
-            self.quantity = value
+            self._quantity = value
 
     def __str__(self) -> str:
         return f"{self.name}: {self.quantity} {self.unit}"
